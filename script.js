a706559805181911f78cc92a648811d798e0426c
Frhack086 <Frhack086@gmail.com>
2023-04-22 14:24:07 -0400

    changes

--- script.js
+++ script.js
@@ -1,178 +1,106 @@
-let area1 = document.querySelector("#mensaje");
-let area2 = document.querySelector("#resultado");
-let area3 = document.querySelector("#mensaje-vacio");
+function encriptar() {
 
-let key;
+	let texto = document.getElementById("encrypt-text-area").value;
 
-// El vector IV tiene que ser lo mismo
-const iv = window.crypto.getRandomValues(new Uint8Array(12));
+	let resultado = document.getElementById("resultado");
 
-async function generateAesGcmKey() {
-  return await window.crypto.subtle.generateKey(
-    {
-      name: "AES-GCM",
-      length: 256,
-    },
-    true,
-    ["encrypt", "decrypt"]
-  );
+	let textoCifrado = texto
+		.replace(/e/gi, "enter")
+		.replace(/i/gi, "imes")
+		.replace(/a/gi, "ai")
+		.replace(/o/gi, "ober")
+		.replace(/u/gi, "ufat");
+
+
+	if (texto.length != 0) {
+		//tituloMensaje.textContent = "texto encriptado con exito"
+		if (validador(texto)) {
+			return;
+		}
+
+		mostrarResultado();
+		resultado.innerText = textoCifrado;
+		//muñeco.src="./img/encriptado.jpg";
+	} else {
+		//muñeco.src ="./img/muñeco.png";
+		alert("debes ingresar algun texto");
+	}
 }
 
-// La llave se genera al inicio del archivo y despues no se vuelve a generar mas hasta que se recargue la pagina
-(async () => {
-  key = await generateAesGcmKey();
-  console.log('CryptoKey:', key);
-})();
-
-window.addEventListener("input", () => {
-  ajustarAlturaTextArea(area1);
-});
-
-window.addEventListener("resize", () => {
-  ajustarAlturaTextArea(area1);
-});
-
-function ajustarAlturaTextArea(textArea) {
-  textArea.style.height = "auto";
-  textArea.style.height = `${textArea.scrollHeight}px`;
+function mostrarResultado() {
+	let x = document.getElementById("resultado-mensaje");
+	let y = document.getElementById("mensaje-por-defecto");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+		y.style.display = "none";
+	} else {
+		x.style.display = "none";
+		y.style.display = "block";
+	}
 }
 
-async function encriptar() {
-    mensaje = document.getElementById("mensaje").value;
-    let  mensajeEncriptado;
+function copiarPortapapeles() {
+	// Get the text field
+	var copyText = document.getElementById("resultado");
 
-    mensaje ? (async () => {
-      mensajeEncriptado = mensaje;
+	// Copy the text inside the text field
+	navigator.clipboard.writeText(copyText.innerText);
 
-      console.log(mensajeEncriptado);
-      const enc = new TextEncoder();
-      const encryptedMessage = await encryptMessage( enc.encode(mensajeEncriptado));
-      console.log('Encrypted message:', encryptedMessage);
-      
-      document.getElementById("resultado").innerHTML = encryptedMessage;
-      document.getElementById("mensaje").value = "";
-      muestraAreas("flex","none");
-      ajustarAlturaTextArea(area1);
-      ajustarAlturaTextArea(area2);
+	// Alert the copied text
+	alert("Copied the text: " + copyText.innerText);
 
-    }) () : (() => {
-      notifica("No existe ningun mensaje");
-
-    })();
-    return mensajeEncriptado;
-
+	mostrarResultado();
 }
 
-async function desencriptar() {
-    mensaje = document.getElementById("mensaje").value;
-    let mensajeDesencriptado;
+function validador(texto) {
+	let validador = false;
+	const caracteresEspciales = /[`!@#$%^&*´()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
-    mensaje ? (async () => { 
+	if (caracteresEspciales.test(texto)) {
+		alert("Tiene caracteres especiales")
+		validador = true;
+	}
 
-      mensajeDesencriptado = mensaje;
-      
-      console.info(mensajeDesencriptado);
-      const encryptedData = base64StringToArrayBuffer(mensajeDesencriptado);
-      const decryptedMessage = await decryptMessage(encryptedData);
+	const mayusculas = /[A-Z]/;
+	if (mayusculas.test(texto)) {
+		alert("Tiene mayusculas especiales")
+		validador = true;
+	}
 
-      document.getElementById("resultado").innerHTML =  decryptedMessage;
-      document.getElementById("mensaje").value = "";
-      muestraAreas("flex","none");
-      ajustarAlturaTextArea(area1);
-      ajustarAlturaTextArea(area2);
+	const acentos = /[à-ü]|[À-Ü]/;
+	if (acentos.test(texto)) {
+		alert("Tiene acentos")
+		validador = true;
+	}
 
-    }) () : (() => {
-      notifica("No existe ningun mensaje");
-
-    })();
-        return mensajeDesencriptado;
+	return validador;
 }
 
-function copiar() {
-    const mensaje = document.getElementById("resultado").textContent;
-    navigator.clipboard.writeText(mensaje)
-    .then(() => {
-      document.getElementById("resultado").textContent = "";
-      notifica("Se copio el texto");
-      ajustarAlturaTextArea(area2);
-      muestraAreas("none","flex");        
-    })
-    .catch(err => {
-      console.error('No se pudo copiar el resultado al portapapeles: ', err);
-    });
+function desencriptar() {
+
+	let texto = document.getElementById("encrypt-text-area").value;
+
+	let resultado = document.getElementById("resultado");
+
+	let textoCifrado = texto
+		.replace(/enter/gi, "e")
+		.replace(/imes/gi, "i")
+		.replace(/ai/gi, "a")
+		.replace(/ober/gi, "o")
+		.replace(/ufat/gi, "u");
+
+	if (texto.length != 0) {
+		//tituloMensaje.textContent = "texto encriptado con exito"
+
+		if (validador(texto)) {
+			return;
+		}
+
+		mostrarResultado();
+		resultado.innerText = textoCifrado;
+		//muñeco.src="./img/encriptado.jpg";
+	} else {
+		//muñeco.src ="./img/muñeco.png";
+		alert("debes ingresar algun texto");
+	}
 }
-
-function muestraAreas(dArea2, dArea3){
-  let copiar =document.getElementById("copiar");
-
-  area2.style.display=dArea2;
-  area3.style.display=dArea3;
-  if(dArea3 == "none") {
-    copiar.style.display="block";
-  } else {
-    copiar.style.display="none";
-  }
-}
-
-function notifica(mensaje) {
-  const contenedorNotificacion = document.getElementById("contenedor-notificacion");
-  const notificacion = document.getElementById("notificacion");
-  notificacion.innerHTML = mensaje;
-    if(contenedorNotificacion.style.display == "block") {
-    }else{
-      contenedorNotificacion.style.display = "block";
-      contenedorNotificacion.style.animation = "slide-down 0.5s ease-in-out forwards";
-      setTimeout(function() {
-        contenedorNotificacion.style.animation = "fade-out 0.5s ease-in-out forwards";
-    
-        setTimeout(function() {
-          contenedorNotificacion.style.display = "none";
-        }, 500);
-      }, 1500);
-    }
-  }
-
-  async function encryptMessage(encodedMessage) {
-    // iv will be needed for decryption
-    return arrayBufferToBase64String(await window.crypto.subtle.encrypt(
-      { name: "AES-GCM", iv: iv },
-      key,
-      encodedMessage
-    ));
-  }
-
-  async function decryptMessage(encryptedText) {
-    return arrayBufferToString(await window.crypto.subtle.decrypt(
-      {
-        name: "AES-GCM",
-        iv: iv
-      },
-      key,
-      encryptedText
-    ));
-  }
-
-  function arrayBufferToBase64String(arrayBuffer) {
-    const byteArray = new Uint8Array(arrayBuffer);
-    let byteString = '';
-    for (let i = 0; i < byteArray.byteLength; i++) {
-      byteString += String.fromCharCode(byteArray[i]);
-    }
-    return btoa(byteString);
-  }
-
-
-  function base64StringToArrayBuffer(base64String) {
-    const byteString = atob(base64String);
-    const byteArray = new Uint8Array(byteString.length);
-    for (let i = 0; i < byteString.length; i++) {
-      byteArray[i] = byteString.charCodeAt(i);
-    }
-    return byteArray.buffer;
-  }
-  
-  function arrayBufferToString(arrayBuffer) {
-    const dec = new TextDecoder();
-    return dec.decode(arrayBuffer);
-  }
-  
